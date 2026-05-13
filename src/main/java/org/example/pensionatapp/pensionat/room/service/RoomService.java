97336d368e29dda2c0ec9e7f91645b5f802f56e2
Linda Bergin <linber24@gmail.com>
2026-05-13 11:51:06 +0200

    Made Methods createRoom and getRoomById

--- src/main/java/org/example/pensionatapp/pensionat/room/service/RoomService.java
+++ src/main/java/org/example/pensionatapp/pensionat/room/service/RoomService.java
@@ -1,6 +1,7 @@
 package org.example.pensionatapp.pensionat.room.service;
 
 import org.example.pensionatapp.pensionat.booking.repository.BookingRepository;
+import org.example.pensionatapp.pensionat.error.NotFoundException;
 import org.example.pensionatapp.pensionat.room.model.Room;
 import org.example.pensionatapp.pensionat.room.repository.RoomRepository;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,17 @@ public class RoomService {
 
     public List<Room> getAllRooms() {
         return roomRepository.findAll();
+    }
+
+    public Room createRoom (String roomNumber, int beds, int pricePerNight) {
+
+        Room room = new Room (roomNumber, beds, pricePerNight);
+
+        return roomRepository.save(room);
+    }
+
+    public Room getRoomById (long id) {
+         return roomRepository.findById(id).orElseThrow(()
+                 ->new NotFoundException("Rum med id " + id + " hittades inte."));
     }
 }
