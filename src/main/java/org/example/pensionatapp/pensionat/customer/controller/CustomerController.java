c0443146812ffb7adaa0095be076785b6a5848ee
David Hasenson <90691386+davidhasenson@users.noreply.github.com>
2026-05-15 16:36:17 +0200

    Added basic functionality to delete customer.

--- src/main/java/org/example/pensionatapp/pensionat/customer/controller/CustomerController.java
+++ src/main/java/org/example/pensionatapp/pensionat/customer/controller/CustomerController.java
@@ -46,4 +46,11 @@ public class CustomerController {
     }
 
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/org/example/pensionatapp/pensionat/customer/service/CustomerService.java
+++ src/main/java/org/example/pensionatapp/pensionat/customer/service/CustomerService.java
@@ -1,11 +1,13 @@
 package org.example.pensionatapp.pensionat.customer.service;
 
 import jakarta.transaction.Transactional;
+import org.apache.coyote.Response;
 import org.example.pensionatapp.pensionat.booking.repository.BookingRepository;
 import org.example.pensionatapp.pensionat.customer.model.CreateCustomerRequest;
 import org.example.pensionatapp.pensionat.customer.model.Customer;
 import org.example.pensionatapp.pensionat.customer.repository.CustomerRepository;
 import org.example.pensionatapp.pensionat.error.NotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,5 +40,17 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
+    public void deleteCustomer(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(()-> new NotFoundException("Kunden hittades inte"));
+
+//        boolean hasBooking = customerRepository.
+//
+//        if (hasBooking) {
+//            throw new IllegalStateException("Kunden har aktiva bokningar");
+//        }
+
+        customerRepository.delete(customer);
+    }
 
 }
