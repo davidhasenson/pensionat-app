3d8c38e0bbe32058856eea17781a59ef1476bc25
Linda Bergin <linber24@gmail.com>
2026-05-18 10:17:36 +0200

    created deleteRoom in RoomService

--- src/main/java/org/example/pensionatapp/pensionat/room/service/RoomService.java
+++ src/main/java/org/example/pensionatapp/pensionat/room/service/RoomService.java
@@ -1,6 +1,8 @@
 package org.example.pensionatapp.pensionat.room.service;
 
+import org.example.pensionatapp.pensionat.booking.BookingStatus;
 import org.example.pensionatapp.pensionat.booking.repository.BookingRepository;
+import org.example.pensionatapp.pensionat.error.BadRequestException;
 import org.example.pensionatapp.pensionat.error.NotFoundException;
 import org.example.pensionatapp.pensionat.room.model.Room;
 import org.example.pensionatapp.pensionat.room.repository.RoomRepository;
@@ -12,9 +14,11 @@ import java.util.List;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final BookingRepository bookingRepository;
 
-    public RoomService(RoomRepository roomRepository, BookingRepository bookingRepository) {
+    public RoomService(RoomRepository roomRepository, BookingRepository bookingRepository, BookingRepository bookingRepository1) {
         this.roomRepository = roomRepository;
+        this.bookingRepository = bookingRepository1;
     }
 
     public List<Room> getAllRooms() {
@@ -35,13 +39,21 @@ public class RoomService {
 
     public Room updateRoom (long id, String roomNumber, int beds, int pricePerNight) {
 
-        Room room = roomRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Rum med id " + id + " hittades inte."));
+       Room room = getRoomById(id);
 
         room.setRoomNumber(roomNumber);
         room.setBeds(beds);
         room.setPricePerNight(pricePerNight);
 
         return roomRepository.save(room);
+    }
+
+    public void deleteRoom (long id) {
+        Room room = getRoomById(id);
+
+       if (!bookingRepository.findByRoomIdAndStatus(id, BookingStatus.ACTIVE).isEmpty()){
+            throw new BadRequestException("Rummet har aktiva bokningar och kan inte tas bort.");
+       }
+            roomRepository.delete(room);
     }
 }
