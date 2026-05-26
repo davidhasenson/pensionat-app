6ff25252fbd6643bfe99ede633db938916ced753
Linda Bergin <linber24@gmail.com>
2026-05-26 14:29:12 +0200

    created logger in RoomController

--- src/main/java/org/example/pensionatapp/pensionat/room/controller/RoomController.java
+++ src/main/java/org/example/pensionatapp/pensionat/room/controller/RoomController.java
@@ -6,6 +6,8 @@ import org.example.pensionatapp.pensionat.room.model.Room;
 import org.example.pensionatapp.pensionat.room.model.RoomResponse;
 import org.example.pensionatapp.pensionat.room.model.UpdateRoomRequest;
 import org.example.pensionatapp.pensionat.room.service.RoomService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,8 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class RoomController {
 
+    private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
+
     private final RoomService roomService;
 
     public RoomController(RoomService roomService) {
@@ -25,16 +29,19 @@ public class RoomController {
 
     @GetMapping
     public List<RoomResponse> getAllRooms() {
+        logger.info("Getting all rooms");
         return roomService.getAllRooms();
     }
 
     @GetMapping("/{id}")
     public Room getRoomById(@PathVariable long id) {
+        logger.info("Getting room with id {}", id);
         return roomService.getRoomById(id);
     }
 
     @PostMapping
     public Room createRoom(@RequestBody @Valid CreateRoomRequest request) {
+        logger.info("Creating room {}", request);
         return roomService.createRoom(
                 request.roomNumber(),
                 request.beds(),
@@ -44,6 +51,7 @@ public class RoomController {
 
     @PutMapping("/{id}")
     public Room updateRoom(@PathVariable long id, @RequestBody @Valid UpdateRoomRequest request) {
+        logger.info("Updating room {}", request);
         return roomService.updateRoom(
                 id,
                 request.roomNumber(),
@@ -54,12 +62,13 @@ public class RoomController {
 
     @DeleteMapping("/{id}")
     public void deleteRoom(@PathVariable long id) {
+        logger.info("Deleting room {}", id);
         roomService.deleteRoom(id);
     }
 
     @GetMapping("/available")
     public ResponseEntity<List<RoomResponse>> getAvailableRooms(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
-      //  logger.info("Received request to fetch available rooms between {} and {}", startDate, endDate);
+        logger.info("Received request to fetch available rooms between {} and {}", startDate, endDate);
         List<RoomResponse> availableRooms = roomService.findAvailableRooms(startDate, endDate);
         return ResponseEntity.ok().body(availableRooms);
     }
