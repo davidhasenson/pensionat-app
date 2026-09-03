bad51202773f42e0b1b7feeaa8b848381c93d114
David Hasenson <davidhasenson@gmail.com>
2026-09-03 10:07:35 +0200

    not working

--- src/main/java/org/example/pensionatapp/pensionat/customer/client/CustomerDto.java
+++ src/main/java/org/example/pensionatapp/pensionat/customer/client/CustomerDto.java
@@ -2,7 +2,7 @@ package org.example.pensionatapp.pensionat.customer.client;
 
 public record CustomerDto(
         Long id,
-        String username,
+        String userName,
         String firstName,
         String lastName,
         String email,
