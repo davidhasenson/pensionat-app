3e4ee14ef37ce9c7795239eb60297f017d178f18
David Hasenson <davidhasenson@gmail.com>
2026-09-07 12:16:12 +0200

    updated how .env is loaded- #36

--- src/main/java/org/example/pensionatapp/PensionatAppApplication.java
+++ src/main/java/org/example/pensionatapp/PensionatAppApplication.java
@@ -11,7 +11,11 @@ public class PensionatAppApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        dotenv.entries().forEach(entry ->{
+            if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
 
         SpringApplication.run(PensionatAppApplication.class, args);
     }
